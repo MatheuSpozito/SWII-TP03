605ad9d255e36b9cf1d5767960f8f93167f3e7af
Matheus Campos <matheus.campos@aluno.ifsp.edu.br>
2021-11-11 17:22:13 -0300

    add application

---
+++ TP03_SWII/Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace TP03_SWII.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<BL> Bls { get; set; }
+        public DbSet<Container> Containers { get; set; }
+    }
+}
